5e26609c7ae431aee6e76872872064bc1f4829a5
Helge Betzinger <helge.betzinger@googlemail.com>
2014-10-17 18:42:12 +0200

    add exception support again

--- coasync4qt/src/awaitables4qt.h
+++ coasync4qt/src/awaitables4qt.h
@@ -3,12 +3,6 @@
 #include "awaitables.h" 
 #include <QFuture>
 
-//public Task < FutureValue > {
-//	FutureValue get() {
-//		// any link to the derived class ... 
-//	}
-//};
-
 class Awaitable : boost::noncopyable {
 
 public:
@@ -22,35 +16,50 @@ public:
 	virtual void get( void * ) = 0;
 
 	void awaitReady() { 
-		if ( isReady()) {
+		try {
+			if (isReady()) {
+			}
+			else if (co_stack().empty()) {
+				assert(!"called task::get outside of a coroutine context! Use a TaskFactory to fix this!");
+			}
+			else {
+				// here we are within coroutine 
+				auto current_coro = co_stack().top();
+				onReady(std::bind([current_coro]()
+				{
+					// We are within the context of async operation and have its result here within 'ready'. Current thread
+					// need not to be the original thread , its simple the thread that was used by the async operation. 
+
+					// Because the main routine still activated "then" and thus now awaits the result urgently, we have now to 
+					// wake-up the coroutine within the original thread. 
+
+					post2thread(
+						current_coro.context,
+						[current_coro] {
+						co_stack().push(std::move(current_coro));
+						co_stack().resume();
+						co_stack().pop();
+					});
+
+				}));
+
+				co_stack().yield(); // back to the caller, because the result is not yet available ... 
+			}
 		}
-		else if (co_stack().empty()) {
-			assert(!"called task::get outside of a coroutine context! Use a TaskFactory to fix this!");
-		}
-		else {
-			// here we are within coroutine 
-			auto current_coro = co_stack().top();
-			onReady( std::bind([current_coro]()
-			{
-				// We are within the context of async operation and have its result here within 'ready'. Current thread
-				// need not to be the original thread , its simple the thread that was used by the async operation. 
-
-				// Because the main routine still activated "then" and thus now awaits the result urgently, we have now to 
-				// wake-up the coroutine within the original thread. 
-
-				post2thread(
-					current_coro.context,
-					[current_coro] {
-					co_stack().push(std::move(current_coro));
-					co_stack().resume();
-					co_stack().pop();
-				});
-
-			}));
-
-			co_stack().yield(); // back to the caller, because the result is not yet available ... 
+		catch (std::exception_ptr& eptr) {
+			std::swap( mException, eptr );
 		}
 	}
+
+	void try_rethrow_exception() {
+		if (mException) {
+			std::rethrow_exception(mException);
+		}
+	}
+
+private: 
+
+	std::exception_ptr mException; 
 };
 
 template< typename FutureValue > struct Task  { 
@@ -81,6 +90,7 @@ template< typename FutureValue > struct Task  {
 		if (!mAwaitable->isReady()) {
 			mAwaitable->awaitReady();
 		}
+		mAwaitable->try_rethrow_exception();
 		// TODO: probably this destroys return value optimization of the compiler: 
 		Result r;
 		mAwaitable->get(&r);
@@ -153,6 +163,7 @@ auto operator << (const TaskFactory& /*dummy*/, Future &&ft) -> typename std::en
 {
 	QFutureAwaitable< typename remove_const_reference< typename Future>::type > task( std::move(ft));
 	task.awaitReady();
+	task.try_rethrow_exception();
 	return task.get();
 }
 
